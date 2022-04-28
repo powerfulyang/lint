98f70bb79f91a17383551875e2cb4c18c020c707
powerfulyang <i@powerfulyang.com>
2022-04-28 20:49:38 +0800

    fix: npm@8 下 peerDependencies 处理异常

--- scripts/install-peers.mjs
+++ scripts/install-peers.mjs
@@ -3,6 +3,7 @@ import { join } from 'path';
 import { pathToFileURL } from 'url';
 import { createRequire } from 'module';
 import { execSync } from 'child_process';
+import { readFileSync, rmSync, writeFileSync } from 'fs';
 
 const dir = process.cwd();
 const path = join(dir, 'package.json');
@@ -13,7 +14,13 @@ const peers = pkg.peerDependencies || {};
 
 Object.keys(peers).forEach((peer) => {
   try {
-    execSync(`npm install ${peer}@latest --no-save --no-package-lock`);
+    // npm@8 的 BUG, Cannot read properties of undefined (reading 'spec')
+    // 要先删除 package.json
+    const tmp = readFileSync(path, { encoding: 'utf8' });
+    rmSync(path);
+    execSync(`npm install ${peer}@latest --no-save`);
+    // 再恢复 package.json
+    writeFileSync(path, tmp);
     console.log(`Installed peer dependency ${peer}`);
   } catch {
     console.error(`Error: installing peer dependency ${peer}`);
