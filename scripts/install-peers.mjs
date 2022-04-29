53e4ed4bc83383edcd7c4e49d1480e69de8fb3a2
powerfulyang <i@powerfulyang.com>
2022-04-29 10:50:37 +0800

    fix: npm 至少在现在是垃圾

--- scripts/install-peers.mjs
+++ scripts/install-peers.mjs
@@ -3,7 +3,6 @@ import { join } from 'path';
 import { pathToFileURL } from 'url';
 import { createRequire } from 'module';
 import { execSync } from 'child_process';
-import { readFileSync, rmSync, writeFileSync } from 'fs';
 
 const dir = process.cwd();
 const path = join(dir, 'package.json');
@@ -14,13 +13,8 @@ const peers = pkg.peerDependencies || {};
 
 Object.keys(peers).forEach((peer) => {
   try {
-    // npm@8 的 BUG, Cannot read properties of undefined (reading 'spec')
-    // 要先删除 package.json
-    const tmp = readFileSync(path, { encoding: 'utf8' });
-    rmSync(path);
+    // npm@8, Cannot read properties of undefined (reading 'spec')
     execSync(`npm install ${peer}@latest --no-save`);
-    // 再恢复 package.json
-    writeFileSync(path, tmp);
     console.log(`Installed peer dependency ${peer}`);
   } catch {
     console.error(`Error: installing peer dependency ${peer}`);
