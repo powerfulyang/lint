d5baf356d17832c886bc1513f29131bd9c11d872
powerfulyang <i@powerfulyang.com>
2022-05-05 14:19:32 +0800

    feat(jest): set Jest to v28
    BREAKING CHANGE: **Jest@28**

--- jest.config.js
+++ jest.config.js
@@ -4,12 +4,14 @@ const tsconfig = require('./tsconfig.json');
 const moduleNameMapper = pathsToModuleNameMapper(tsconfig.compilerOptions.paths, {
   prefix: '<rootDir>/',
 });
+
+/**
+ * @type {import('@jest/types').Config.InitialOptions}
+ */
 module.exports = {
   moduleNameMapper,
   moduleFileExtensions: ['ts', 'tsx', 'js'],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
+  preset: 'ts-jest',
   testMatch: ['**/*.spec.(ts|tsx)'],
   testPathIgnorePatterns: ['./node_modules/', './dist/'],
   testEnvironment: 'jsdom',
